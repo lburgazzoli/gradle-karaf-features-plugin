6a689d9847226eb4bd54ea39b93143265bfb060a
Siarhei_Nekhviadovich <Siarhei_Nekhviadovich@epam.com>
2015-11-28 11:17:53 +0300

    Fix project adding to feature

--- src/main/groovy/com/github/lburgazzoli/gradle/plugin/karaf/features/model/FeatureDescriptor.groovy
+++ src/main/groovy/com/github/lburgazzoli/gradle/plugin/karaf/features/model/FeatureDescriptor.groovy
@@ -93,7 +93,7 @@ class FeatureDescriptor {
 				ConfigureUtil.configure( closure, projectDescriptor )
 			}
 			this.project.logger.debug("Add project '${project.name}' to feature '${this.name}'");
-			if ( this.projectDescriptors) {
+			if ( this.projectDescriptors == null ) {
 				this.projectDescriptors = [ projectDescriptor ]
 			} else {
 				this.projectDescriptors += projectDescriptor
